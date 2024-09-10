ea638574fe0a701e1c29a6ab13bbfd4a54763c5a
MichaelAntipov <111461750+MichaelAntipov@users.noreply.github.com>
2024-09-10 13:40:45 -0500

    Update WordAnalysis.java

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -1,9 +1,5 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.*;
 
 /**
  * This program checks which words in a file are not present in a dictionary.
@@ -13,6 +9,30 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Read the dictionary file and the novel file
+        Set<String> dictionaryWords = readWords("./src/words");
+        Set<String> novelWords = readWords("./src/war-and-peace.txt");
+
+        for(String word: novelWords){
+            if(!dictionaryWords.contains(word)){
+                System.out.println(word);
+            }
+        }
+
+        // print out the number of unique words in the novel
+        System.out.println("There are "+novelWords.size() + " unique words in the novel.");
+
+        // print the number of unique words with >3 letters
+
+        Iterator<String> iterator = novelWords.iterator();
+        while(iterator.hasNext()){
+            if(iterator.hasNext()){
+                iterator.remove();
+            }
+        }
+
+        System.out.println("There are " + novelWords.size() + " unique words with more than 3 letters");
+        
     }
 
     /**
@@ -25,6 +45,25 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
+        // We use a HashSet instead of a TreeSet because the order doesn't matter
+        Set<String> words = new HashSet<>();
+
+        // deterine the current working directory
+        //System.out.println(System.getProperty("user.dir"));
+
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+        
+        // Use any character that's not a letter as a delimeter
+
+        in.useDelimiter("[^a-zA-Z]+");
+
+        while (in.hasNext()){
+            //Add words to the set (duplicates are automaticaly ignored)
+            words.add(in.next().toLowerCase());
+        }
+
+
+
         return null;
     }
 }
