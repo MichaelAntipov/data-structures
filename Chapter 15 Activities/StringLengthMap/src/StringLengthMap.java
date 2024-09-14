d850b4a4c2e853d751e18b67a03df307ac9d009d
antimic <mkantipov@stu.naperville203.org>
2024-09-14 18:38:04 -0500

    Finished the String lengthmap labs

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -14,66 +14,45 @@ public class StringLengthMap
     {
         String filename = "Chapter 15 Activities\\StringLengthMap\\src\\test1.txt";
 
-        
-
-
-        Map<Integer, String> strlength = new HashMap<>();
-        Map<Integer, String> strlength1 = new HashMap<>();
-        //int len1 = 0;
         try (Scanner in = new Scanner(new File(filename)))
         {
 
             // Create your map here
-            
-            
+            Map<Integer, String> map = new TreeMap<>();
+
             while (in.hasNext())
             {
                 String word = clean(in.next());
                 Integer len = word.length();
-                
-                
-                if(strlength.get(len) == "" || strlength.get(len) == null){
-                    strlength.put(len,word);
-                }
-                else{
-                    String s = strlength.get(len);
-                    s = s +", " + word;
-                    strlength.put(len, s);
-                }
-                
-                   //len1 = len;
-                
-                /* else if(strlength.size() > 0){
-                    for(int i = 0; i < strlength.size(); i++){
-                        if(len == strlength.)
-                    }
-                }*/
 
                 // Update the map here
                 // Modify Worked Example 15.1
-
-
-                
+                if (map.containsKey(len))
+                {
+                    String existing = map.get(len);
+                    existing = existing + ", " + word;
+                    map.put(len, existing);
+                }
+                else
+                {
+                    map.put(len, word);
+                }
 
 
             }
 
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
+
+            for (Integer key : map.keySet())
+            {
+                System.out.println(key + ": " + map.get(key));
+            }
+            
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
         }
-
-        for(int i =0; i < 16;i++){
-            strlength1.put(i,strlength.get(i));
-        }
-
-        for(int i = 0; i< 16; i++){
-            if()
-                System.out.println(strlength1.get(i));
-        }
-        
     }
 
     public static String clean(String s)
