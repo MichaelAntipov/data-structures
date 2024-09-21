11d899ef43d0dcd6931a55a66d6bc83772ef29c5
antimic <mkantipov@stu.naperville203.org>
2024-09-21 14:42:55 -0500

    finished sentence reverser

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -2,27 +2,60 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * Class for reversing the order of a sentence.
-*/
+ * Class for reversing the order of words in sentences.
+ */
 public class SentenceReverser
 {
     /**
-     * Reverses the given sentence.
+     * Reverses the words in each sentence of the given text.
      *
-     * @param sentence Sentence to be reversed.
-     * @return reversed sentence.
-    */
+     * @param sentence Sentence(s) to be reversed.
+     * @return Reversed sentence(s).
+     */
     public static String reverse(String sentence)
     {
-    	Scanner scanner = new Scanner(sentence);
-    	
-        // Complete this method. Use a Stack.
-        ...
+        Scanner scanner = new Scanner(sentence);
+        Stack<String> stack = new Stack<>(); 
+        String reversedSentence = ""; 
 
+        while (scanner.hasNext()) {
+            String word = scanner.next();
 
+            stack.push(word.toLowerCase()); 
 
+            if (word.endsWith(".")) { 
+                
+                String lastWord = stack.pop();
+                lastWord = lastWord.substring(0, lastWord.length() - 1);
+                stack.push(lastWord); 
 
+                String tempSentence = "";
 
+                while (!stack.isEmpty()) {
+                    String currentWord = stack.pop();
+                    if (tempSentence.isEmpty()) {
+                        currentWord = currentWord.substring(0, 1).toUpperCase() + currentWord.substring(1);
+                    }
+                    tempSentence += currentWord;
+                    if (!stack.isEmpty()) {
+                        tempSentence += " "; 
+                    }
+                }
 
+                tempSentence += "."; 
+
+                reversedSentence += tempSentence;
+
+                if (scanner.hasNext()) {
+                    reversedSentence += " "; 
+                }
+
+                stack.clear();
+            }
+        }
+
+        scanner.close(); 
+
+        return reversedSentence.trim(); 
     }
 }
